0cf9f1a989c96f5533228c630e53ec292367727e
Sven <glaglasven@live.com>
2019-11-21 08:42:32 +0100

    Sven Test Data Cleaning Hough

--- src/plot_functions.py
+++ src/plot_functions.py
@@ -4,6 +4,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.utils.multiclass import unique_labels
+import numpy as np
 
 
 def plot_confusion_matrix(y_true, y_pred,
