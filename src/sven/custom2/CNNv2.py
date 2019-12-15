abfab84b9b81bdacfb47d64a0cc9d5f6133c4819
Valentin Viennot <valentin.viennot@epfl.ch>
2019-12-15 12:44:15 +0100

    separate unet model from CNN class

--- src/sven/custom2/CNNv2.py
+++ src/sven/custom2/CNNv2.py
@@ -1,7 +1,8 @@
 import os
 
-from keras.models import *
-from keras.layers import *
+import numpy as np
+
+from keras.layers import Input
 from keras.optimizers import *
 from keras.utils import np_utils
 from keras.regularizers import l2
@@ -9,6 +10,7 @@ from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 from keras import losses
 
 from helpers import *
+from unet import unet
 
 import imgaug as ia
 import imgaug.augmenters as iaa
@@ -53,54 +55,9 @@ class CNN:
         dropout_rate = self.dropout_rate
         #lk_alpha = 0.1
         
-        # TODO: LeakyReLU(alpha=0.1) is better than relu (cf. lectures)
         inputs = Input((self.window_size, self.window_size, self.channels_size))
-        conv1 = Conv2D(nb_conv_1, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-        conv1 = Conv2D(nb_conv_1, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-        
-        pool1 = MaxPooling2D(pool_size=pool_size)(conv1)
-        conv2 = Conv2D(nb_conv_2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-        conv2 = Conv2D(nb_conv_2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-        
-        pool2 = MaxPooling2D(pool_size=pool_size)(conv2)
-        conv3 = Conv2D(nb_conv_3, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-        conv3 = Conv2D(nb_conv_3, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-        
-        pool3 = MaxPooling2D(pool_size=pool_size)(conv3)
-        conv4 = Conv2D(nb_conv_4, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-        conv4 = Conv2D(nb_conv_4, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-        drop4 = Dropout(dropout_rate)(conv4)
-        
-        pool4 = MaxPooling2D(pool_size=pool_size)(drop4)
-        conv5 = Conv2D(nb_conv_5, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-        conv5 = Conv2D(nb_conv_5, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-        drop5 = Dropout(dropout_rate)(conv5)
-
-        up6 = Conv2D(nb_conv_4, upconv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = pool_size)(drop5))
-        merge6 = concatenate([drop4, up6], axis = 3)
-        conv6 = Conv2D(nb_conv_4, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-        conv6 = Conv2D(nb_conv_4, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-
-        up7 = Conv2D(nb_conv_3, upconv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = pool_size)(conv6))
-        merge7 = concatenate([conv3, up7], axis = 3)
-        conv7 = Conv2D(nb_conv_3, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-        conv7 = Conv2D(nb_conv_3, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-
-        up8 = Conv2D(nb_conv_2, upconv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = pool_size)(conv7))
-        merge8 = concatenate([conv2, up8], axis = 3)
-        conv8 = Conv2D(nb_conv_2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-        conv8 = Conv2D(nb_conv_2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-
-        up9 = Conv2D(nb_conv_1, upconv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = pool_size)(conv8))
-        merge9 = concatenate([conv1, up9], axis = 3)
-        conv9 = Conv2D(nb_conv_1, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-        conv9 = Conv2D(nb_conv_1, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-        conv9 = Conv2D(2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-        
-        # final segmentation layer: pixel-wise classifier
-        conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
-
-        self.model = Model(input = inputs, output = conv10) # TODO: ensure this is doing pixel-wise segmentation
+        self.model = unet(inputs, dropout_rate, pool_size,
+            conv_size, upconv_size, nb_conv_1, nb_conv_2, nb_conv_3, nb_conv_4, nb_conv_5)
 
         self.model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
         # TODO: use better metrics? 
---
+++ src/sven/custom2/unet.py
@@ -0,0 +1,52 @@
+from keras.models import *
+from keras.layers import *
+
+# TODO: LeakyReLU(alpha=0.1) is better than relu (cf. lectures)
+def unet(inputs, dropout_rate, pool_size,
+        conv_size, upconv_size, nb_conv_1, nb_conv_2, nb_conv_3, nb_conv_4, nb_conv_5):
+    conv1 = Conv2D(nb_conv_1, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(nb_conv_1, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+
+    pool1 = MaxPooling2D(pool_size=pool_size)(conv1)
+    conv2 = Conv2D(nb_conv_2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(nb_conv_2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+
+    pool2 = MaxPooling2D(pool_size=pool_size)(conv2)
+    conv3 = Conv2D(nb_conv_3, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(nb_conv_3, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+
+    pool3 = MaxPooling2D(pool_size=pool_size)(conv3)
+    conv4 = Conv2D(nb_conv_4, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(nb_conv_4, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(dropout_rate)(conv4)
+
+    pool4 = MaxPooling2D(pool_size=pool_size)(drop4)
+    conv5 = Conv2D(nb_conv_5, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(nb_conv_5, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(dropout_rate)(conv5)
+
+    up6 = Conv2D(nb_conv_4, upconv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = pool_size)(drop5))
+    merge6 = concatenate([drop4, up6], axis = 3)
+    conv6 = Conv2D(nb_conv_4, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(nb_conv_4, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2D(nb_conv_3, upconv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = pool_size)(conv6))
+    merge7 = concatenate([conv3, up7], axis = 3)
+    conv7 = Conv2D(nb_conv_3, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(nb_conv_3, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2D(nb_conv_2, upconv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = pool_size)(conv7))
+    merge8 = concatenate([conv2, up8], axis = 3)
+    conv8 = Conv2D(nb_conv_2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(nb_conv_2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv2D(nb_conv_1, upconv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = pool_size)(conv8))
+    merge9 = concatenate([conv1, up9], axis = 3)
+    conv9 = Conv2D(nb_conv_1, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(nb_conv_1, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(2, conv_size, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+
+    # final segmentation layer: pixel-wise classifier
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+    return Model(input = inputs, output = conv10)
