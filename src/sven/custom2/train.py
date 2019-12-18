47fd9c4b16783b4b119333ebe9da600c3b960803
Valentin Viennot <valentin.viennot@epfl.ch>
2019-12-18 23:54:56 +0100

    clean train script

--- src/sven/custom2/train.py
+++ src/sven/custom2/train.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Imports
-
-# In[ ]:
-
-
-MODEL_NAME="V3.001"
-
-
-# In[ ]:
-
+MODEL_NAME="V3.002"
 
 import numpy as np
 import os
@@ -19,21 +10,7 @@ import sys
 from helpers import *
 from CNNv2 import CNN
 
-IN_COLAB = False
-
-
-# # Load images
-
-# In[ ]:
-
-
-# Load the training set
-if (IN_COLAB):
-    from google.colab import drive
-    drive.mount('/gdrive')
-    root_dir = "/gdrive/My Drive/ML/training/"
-else:
-    root_dir = "training/"
+root_dir = "training/"
 
 image_dir = root_dir + "images/"
 files = os.listdir(image_dir)
@@ -46,21 +23,17 @@ print("Loading " + str(n) + " groundtruth")
 gt_imgs = np.asarray([load_image(gt_dir + files[i]) for i in range(n)])
 
 
-# # Load model
-
-# In[ ]:
-
-
 model = CNN(
     rootdir=root_dir,
     window_size=320,
+    net_depth=5,
     lk_alpha=0.1,
     batchnorm=True,
     residual=True,
     nb_epochs=50,
     adjust_metric='val_f1',
-    save_metric='val_f1',
-    stop_metric=None,
+    save_metric='val_iou',
+    stop_metric='f1',
     train_batch_size=4,
     val_batch_size=4,
     random_seed=657,
@@ -69,78 +42,49 @@ model = CNN(
     log_csv_filename='{}.csv'.format(MODEL_NAME),
     use_multiprocessing=False)
 
-
-# # Split dataset
-# 
-# > This step is not necessary when preparing submission but it is to compare different models and/or parameters.
-
-# In[ ]:
-
-
 TEST_PROPORTION = 0.1
 X_train, X_test, y_train, y_test = model.split_data(imgs, gt_imgs, rate=TEST_PROPORTION, seed=42)
 print(X_test.shape[0], 'test images;', X_train.shape[0], 'train images')
 
 
-# ## Train model (and save it)
-
-# In[ ]:
-
-
 #model.load(os.path.join(root_dir, f'{MODEL_NAME}-saved_weights.h5'))
-model.load(os.path.join(root_dir, '{}.h5'.format(MODEL_NAME)))
+#model.load(os.path.join(root_dir, '{}.h5'.format(MODEL_NAME)))
 #model.load(os.path.join(root_dir, 'models', 'another_model.h5'))
-INITIAL_EPOCH = 9
+INITIAL_EPOCH = 0
 
 
-# In[ ]:
-
-
-X_train, y_train = imgs, gt_imgs
+## Train on whole data set
+#X_train, y_train = imgs, gt_imgs
 history = model.train(X_train, y_train, initial_epoch=INITIAL_EPOCH)
-
-
-# In[ ]:
 
 
 model.save(os.path.join(root_dir, '{}-saved_weights.h5'.format(MODEL_NAME)))
 
 
-# In[ ]:
-
-
 def plot_metric(metric_name):
   # Plot training & validation accuracy values
-  plt.plot(history.history[metric_name])
-  plt.plot(history.history['val_{}'.format(metric_name)])
-  plt.title('Model {}'.format(metric_name))
-  plt.ylabel(''+metric_name)
-  plt.xlabel('Epoch')
-  plt.legend(['Train', 'Test'], loc='upper left')
-  plt.savefig(os.path.join(root_dir, '{}-{}.png'.format(MODEL_NAME, metric_name)))
-  plt.show()
+  f = plt.figure()
+  f.plot(history.history[metric_name])
+  f.plot(history.history['val_{}'.format(metric_name)])
+  f.title('Model {}'.format(metric_name))
+  f.ylabel(''+metric_name)
+  f.xlabel('Epoch')
+  f.legend(['Train', 'Test'], loc='upper left')
+  f.savefig(os.path.join(root_dir, '{}-{}.png'.format(MODEL_NAME, metric_name)))
+  f.clear()
+  plt.close(f)
+  #plt.show()
 
 plot_metric('loss')
 plot_metric('acc')
 plot_metric('f1')
 plot_metric('iou')
 
-
-# # Test model
-
-# In[ ]:
-
-
 results = model.test(X_test, y_test)
 print('--- report: saved weights ---')
 print(results)
-
-
-# In[ ]:
-
 
 model.load(os.path.join(root_dir, '{}.h5'.format(MODEL_NAME)))
 results = model.test(X_test, y_test)
 print('--- report: {}.h5 ---'.format(MODEL_NAME))
 print(results)
-
